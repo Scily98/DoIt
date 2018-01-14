0df777be3543bc23fd0d75e63e677c973b4704c3
Morgan Gardiner <scily98@gmail.com>
2018-01-14 17:24:22 +0000

    Add page connected to code

---
+++ DoIt/CreateClassViewController.swift
@@ -0,0 +1,27 @@
+//
+//  CreateClassViewController.swift
+//  DoIt
+//
+//  Created by Morgan Gardiner on 14/01/2018.
+//  Copyright © 2018 Morgan Gardiner. All rights reserved.
+//
+
+import UIKit
+
+class CreateClassViewController: UIViewController {
+    
+    @IBOutlet weak var importantSwitch: UISwitch!
+    @IBOutlet weak var taskNameTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+   
+    @IBAction func addTapped(_ sender: Any) {
+        
+    }
+    
+
+}
