3e631e2fa83b7fb6354def741f36a902bd7b390b
Morgan Gardiner <scily98@gmail.com>
2018-01-14 15:20:44 +0000

    basic

---
+++ DoIt/Task.swift
@@ -0,0 +1,14 @@
+//
+//  Task.swift
+//  DoIt
+//
+//  Created by Morgan Gardiner on 14/01/2018.
+//  Copyright © 2018 Morgan Gardiner. All rights reserved.
+//
+
+import Foundation
+
+class Task {
+    var name = ""
+    var important = false
+}
